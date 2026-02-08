2510bd2a3b2fd58194f1d05b9c2f19dec00006aa
Kennidy-L-Guimaraes <kennidylguimaraes@gmail.com>
2026-02-08 16:20:48 -0300

    Initial Commit: Projeto Ct-e em desenvolvimento.

---
+++ README.md
@@ -0,0 +1,3 @@
+Projeto em desenvolvimento Sistema CT-e.
+Não possui integração SEFAZ.
+Foco em arquitetura, UI e domínio fiscal.
---
+++ project/CargoCTe.dpr
@@ -0,0 +1,19 @@
+program CargoCTe;
+
+uses
+  Vcl.Forms,
+  CargoCTe.View in '..\view\CargoCTe.View.pas' {Frm_CargoCTe},
+  MenuNavigationService in '..\services\MenuNavigationService.pas' {MenuNavigationService.pas},
+  CargoCteConfig.View in '..\view\CargoCteConfig.View.pas' {Frm_CargoCteConfig},
+  CargoCteNovaTransportadora.View in '..\view\CargoCteNovaTransportadora.View.pas' {Frm_NovaTransportadora};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TFrm_CargoCTe, Frm_CargoCTe);
+  Application.CreateForm(TFrm_CargoCteConfig, Frm_CargoCteConfig);
+  Application.CreateForm(TFrm_NovaTransportadora, Frm_NovaTransportadora);
+  Application.Run;
+end.
