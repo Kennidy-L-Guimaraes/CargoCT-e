8479cd443b19b8d2eb10529d569a09bae10cbc97
Kennidy-L-Guimaraes <kennidylguimaraes@gmail.com>
2026-02-18 11:49:56 -0300

    refactor: reorganização total da estrutura de pastas e atualização do .dpr para refletir MVC/DDD

---
+++ CargoCTe.dpr
@@ -0,0 +1,24 @@
+program CargoCTe;
+
+uses
+  Vcl.Forms,
+  CargoCTe.View in 'src\Presentation\Views\CargoCTe.View.pas' {Frm_CargoCTe},
+  CargoCteConfig.View in 'src\Presentation\Views\CargoCteConfig.View.pas' {Frm_CargoCteConfig},
+  CargoCteNovaTransportadora.View in 'src\Presentation\Views\CargoCteNovaTransportadora.View.pas' {Frm_NovaTransportadora},
+  MenuNavigationService in 'src\Presentation\UI\Utils\MenuNavigationService.pas',
+  CampoVisual.Utils in 'src\Presentation\UI\Utils\CampoVisual.Utils.pas',
+  Transportadora.Interfaces in 'src\Domain\Interfaces\Transportadora.Interfaces.pas',
+  Endereco.Tipo.CEP in 'src\Domain\ValueObjects\Endereco.Tipo.CEP.pas',
+  IdentidadeFiscal.Tipo.CNPJ in 'src\Domain\ValueObjects\IdentidadeFiscal.Tipo.CNPJ.pas',
+  IdentidadeFiscal.Entidade in 'src\Domain\Entities\IdentidadeFiscal.Entidade.pas',
+  Endereco.Entidade in 'src\Domain\Entities\Endereco.Entidade.pas';
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TFrm_CargoCTe, Frm_CargoCTe);
+  Application.CreateForm(TFrm_CargoCteConfig, Frm_CargoCteConfig);
+  Application.CreateForm(TFrm_NovaTransportadora, Frm_NovaTransportadora);
+  Application.Run;
+end.
