5a972f0c25ed97a2ac1299f8d58dbd819b61d10f
Kennidy-L-Guimaraes <kennidylguimaraes@gmail.com>
2026-03-09 20:03:18 -0300

    feat(dto): adiciona o DTO Transportadora.DTO

---
+++ src/Application/Dtos/Transportadora.DTO.pas
@@ -0,0 +1,49 @@
+unit Transportadora.DTO;
+
+interface
+
+type
+ TTransportadoraDTO = record
+   Public
+    {Public Declarations}
+
+    //Identificação Fiscal
+    CNPJ               : string;
+    RazaoSocial        : string;
+    InsCricaoEstadual  : string;
+    RegimeTributario   : string;
+
+    //Endereço Fiscal
+    CEP                : string;
+    Municipio          : string;
+    Bairro             : string;
+    Logradouro         : string;
+    CodigoIBGE         : string;
+    Complemento        : string;
+    EstadoUF           : string;
+    Numero             : string;
+
+    //Dados Fiscais
+    PapelCTe          : string;
+    TipoTributacao    : string;
+    ObservacaoFiscal  : string;
+    Aliquota          : Currency;
+
+    //Dados Operacionais
+    TipoTransporte    : string;
+    TipoOperacao      : string;
+
+    //Sistema
+    Status            : string;
+    Responsavel       : string;
+    DataCadastro      : string;
+
+    //Contato
+    Telefone          : string;
+    Site              : string;
+    Email             : string;
+ end;
+
+implementation
+
+end.
