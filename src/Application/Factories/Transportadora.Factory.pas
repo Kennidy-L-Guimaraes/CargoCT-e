c71888bbcd00a056ff19027bc7d8980b3152dd59
Kennidy-L-Guimaraes <kennidylguimaraes@gmail.com>
2026-03-11 13:21:54 -0300

    feat(factory): adiciona Transportadora.Factory com o mapa do dominio DTO

---
+++ src/Application/Factories/Transportadora.Factory.pas
@@ -0,0 +1,74 @@
+unit Transportadora.Factory;
+
+interface
+ uses
+ Validar.Exceptions, Transportadora.DTO, Transportadora.Entidade,
+  DadosFiscais.Entidade, DadosOperacionais.Entidade, Endereco.Entidade,
+  IdentidadeFiscal.Entidade, IdentidadeFiscal.Tipo.CNPJ,
+  SistemaOperacional.Entidade, TransportadoraContato.Entidade, Endereco.Tipo.CEP,
+  RegimeTributario.Mapper, PapelCte.Mapper, TipoTributacao.Mapper;
+
+ type
+ TFactoryTransportadora = class
+   private
+    {Private Declarations}
+   public
+    {Public Declarations}
+   class function CriarTransportadora(DTO: TTransportadoraDTO): TTransportadora;
+ end;
+
+implementation
+
+{ TFactoryTransportadora }
+
+class function TFactoryTransportadora.CriarTransportadora(DTO: TTransportadoraDTO): TTransportadora;
+  var
+  Transportadora    : TTransportadora;
+  DadosFiscais      : TDadosFiscais;
+  DadosOperacionais : TDadosOperacionais;
+  Endereco          : TEndereco;
+  IdentidadeFiscal  : TIdentidadeFiscal;
+  SistemaOperacional: TSistemaOperacional;
+  Contato           : TContato;
+  CEP               : TCEP;
+  CNPJ              : TCNPJ;
+begin
+  {
+   TextoEnum -> Texto para Enum
+   EnumTexto -> Enum para Texto
+  }
+ //Records
+ CEP := TCEP.Create(DTO.CEP);
+ CNPJ:= TCNPJ.Create(DTO.CNPJ);
+ //Identidade FIscal
+ IdentidadeFiscal := TIdentidadeFiscal.Create(
+  CNPJ,
+  DTO.RazaoSocial,
+  DTO.InsCricaoEstadual,
+  TMapperRegimeTributario.TextoEnum(DTO.RegimeTributario));
+
+  //Endereço Fiscal
+  Endereco := TEndereco.Create(
+   CEP,
+   DTO.EstadoUF,
+   DTO.Municipio,
+   DTO.Bairro,
+   DTO.Logradouro,
+   DTO.Numero,
+   DTO.CodigoIBGE,
+   DTO.Complemento);
+
+   //Dados Fiscais
+   DadosFiscais := TDadosFiscais.Create(
+   TMapperPapelCTe.TextoEnum(DTO.PapelCTe),
+   TMapperTipoTributacao.TextoEnum(DTO.TipoTributacao),
+   DTO.ObservacaoFiscal,
+   DTO.Aliquota);
+
+   //Dados Operacinais
+   //DadosOperacionais := TDadosOperacionais.Create();
+
+
+end;
+
+end.
