8311f0aa843a9f1e60e96c671517cf83432d6a10
Kennidy-L-Guimaraes <kennidylguimaraes@gmail.com>
2026-03-12 13:11:56 -0300

    refactor(use-case): remove comentarios sobre (interface e Factory)

--- src/Application/UseCase/NovaTransportadora.UseCase.pas
+++ src/Application/UseCase/NovaTransportadora.UseCase.pas
@@ -2,17 +2,18 @@ unit NovaTransportadora.UseCase;
 
 interface
 
-uses Transportadora.DTO, Validar.Exceptions;
+uses Transportadora.DTO, Validar.Exceptions, TransportadoraRepository.Interfaces,
+  Transportadora.Factory;
 
 type
  TUseCaseNovaTransportadora = class
    private
     {Private Declarations}
-     //FRepository : ITransportadoraRepository;
+     FRepository : ITransportadoraRepository;
 
    public
     {Public Declarations}
-    //constructor Create(ARepository: ITransportadoraRepository);
+    constructor Create(ARepository: ITransportadoraRepository);
     procedure Executar(DTO: TTransportadoraDTO);
  end;
 
@@ -20,17 +21,17 @@ implementation
 
 { TUseCaseNovaTransportadora }
 
-{constructor TUseCaseNovaTransportadora.Create(ARepository: ITransportadoraRepository);
+constructor TUseCaseNovaTransportadora.Create(ARepository: ITransportadoraRepository);
 begin
  FRepository := ARepository;
-end;}
+end;
 
 procedure TUseCaseNovaTransportadora.Executar(DTO: TTransportadoraDTO);
 begin
-  //if FRepository.ExisteNoBanco(DTO.CNPJ) then
-  // TValidar.LancarErro('Transportadora','Inválido, transportadora já registrada!');
-  //Transportadora := TTransportadoraFactory.Criar(DTO);
-  //FRepository.SalvarTransportadora(Transportadora);
+  if FRepository.ExisteNoBanco(DTO.CNPJ) then
+   TValidar.LancarErro('Transportadora','Inválido, transportadora já registrada!');
+   TFactoryTransportadora.CriarTransportadora(DTO);
+   //FRepository.SalvarTransportadora(Transportadora);
 end;
 
 end.
