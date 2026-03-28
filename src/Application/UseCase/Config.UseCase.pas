034a4edb46fa7f0f4cbf223f97c723c5213b9a3a
Kennidy-L-Guimaraes <kennidylguimaraes@gmail.com>
2026-03-28 17:31:30 -0300

    feat(usecase): cria um usecase para as configurações do sistema Config.Usecase

---
+++ src/Application/UseCase/Config.UseCase.pas
@@ -0,0 +1,16 @@
+unit Config.UseCase;
+
+interface
+ type
+  TUsecaseConfig = class
+    private
+     {Private Declarations}
+
+    public
+     {Public Declarations}
+
+  end;
+
+implementation
+
+end.
